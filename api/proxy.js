999b3050ee894ac7023af815a36c7ce62124204f
xrobballerra <xrobballerra80@gmail.com>
2025-12-29 21:53:19 +0000

    proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,9 @@
 // api/proxy.js
 module.exports = async (req, res) => {
   const url = req.query.url;
-  if (!url) return res.status(400).send('No URL provided');
+  if (!url) {
+    return res.status(400).send('No URL provided');
+  }
 
   try {
     const response = await fetch(url, {
@@ -9,12 +11,19 @@ module.exports = async (req, res) => {
       headers: req.headers,
       body: req.body
     });
+
     const data = await response.text();
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // ✅ Always end the response
     res.send(data);
+    res.end();
+
   } catch (e) {
-    res.status(500).send({ error: e.message });
+    // ✅ Send error as JSON
+    res.status(500).json({ error: e.message });
+    res.end();
   }
 };
